3cf93214fb2ef4df69649c886cf3443a37e6a8d6
MynorAlv <malvaradop13@miumg.edu.gt>
2025-11-21 15:01:42 -0600

    Serie 4 - Creacion de la base de datos con las tablas usuario y estadousuario, se realizaron los insert solicitados en la tabla estadousuario

--- Mynor_Alvarado/script.sql
+++ Mynor_Alvarado/script.sql
@@ -1,9 +1,31 @@
 -- Script SQL para evaluación UPA
--- Autor: Mynor Osvaldo Alvarado Pérez
--- Este archivo contendra:
---  - Creacion de la base de datos de la Serie 4
---  - Creacion de tablas de la Serie 4
---  - Inserts iniciales de EstadoUsuario de la Serie 4
---  - Inserts requeridos para reportes de la Serie 7
 
--- El contenido sera agregado en las siguientes series.
+-- 1. Creamos la base de datos con el nombre solicitado
+create database evaluacion_mynor_alvarado;
+use evaluacion_mynor_alvarado;
+
+-- 2. Creamos la tabla de EstadoUsuario
+create table estado_usuario (
+id int auto_increment primary key, 
+titulo varchar(255) not null,
+clave varchar(255) not null 
+);
+
+-- 3. Creamos la tabla usuario
+create table usuario (
+id int auto_increment primary key, 
+nombre varchar(100) not null,
+fecha date not null,
+telefono varchar(20) not null,
+correo varchar(50) not null unique,
+creacion timestamp default current_timestamp,
+estado_usuario_id int not null,
+foreign key (estado_usuario_id) references estado_usuario(id)
+);
+
+-- 3. Insert estaticos de la serie 4
+insert into estado_usuario (id, titulo, clave)
+values ('1', 'Activo', 'activo');
+
+insert into estado_usuario (id, titulo, clave)
+values ('2', 'Baja Permanente', 'baja');
