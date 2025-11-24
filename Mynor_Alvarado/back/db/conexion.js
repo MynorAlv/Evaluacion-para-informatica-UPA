b7e0ae3a24c8f52bf25da4e2c1f69d750c02cec9
Stivo-20 <stevediazf20@gmail.com>
2025-11-24 09:44:40 -0600

    Serie 1 Clonacion de repositorio de la primera parte de la evaluacion se realizo la union entre el repositorio local y remoto

--- Mynor_Alvarado/back/db/conexion.js
+++ Mynor_Alvarado/back/db/conexion.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Ujsd#hfQ@s_15',
     database: 'evaluacion_mynor_alvarado'
 });
 
